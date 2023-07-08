6bb88600e4949cde0b3388ad4169c0650be3ff1a
William Dowling <wmdowling@gmail.com>
2023-07-08 19:56:23 +0200

    Begin description of VEB algorithm

--- fdbserver/include/fdbserver/VEB.h
+++ fdbserver/include/fdbserver/VEB.h
@@ -34,6 +34,24 @@
 #include "fdbserver/IKeyValueContainer.h"
 #include "flow/Arena.h"
 
+// The van Emde Boas tree (https://en.wikipedia.org/wiki/Van_Emde_Boas_tree) is a tree data structure  
+// which implements an associative array with m-bit integer keys. It performs all operations in 
+// O(log m) time assuming an m-bit operation can be completed in constant time.
+//
+// The VEB tree supports the following operations:
+//  Insert: insert key/value pair with an m-bit key
+//  Delete: remove the key/value pair with a given key
+//  Lookup: find the value associated with a given key
+//  FindNext: find the key/value pair with the smallest key which is greater than a given k
+//  FindPrevious: find the key/value pair with the largest key which is smaller than a given k
+//  Min: Returns the minimum element stored in a tree (operates in O(n) time)
+//  Max: Returns the maximum element stored in a tree (operates in O(n) time)
+//
+// Insert(T, x):
+//  Inserts value x in to tree T. 
+//  - If T is empty then T.min = x and T.max = x
+//  - If x < T.min then insert T.min into subtree i responsible for T.min. Set T.min = x.
+
 class veb {
 public:
 	typedef std::size_t size_type;
